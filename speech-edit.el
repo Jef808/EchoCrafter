253b694ffc21b1c4623d5bdd6316c10b1811876b
Jean-Francois Arbour <jf.arbour@gmail.com>
2023-11-30 09:37:50 -0500

    remove old code

--- speech-edit.el
+++ speech-edit.el
@@ -125,42 +125,6 @@ a transcription process was already running."
   (unless (process-live-p speech-edit--transcription-process)
     (message "No recording process is running.")))
 
-(defun generate-emacs-lisp-code (api-key emacs-environment user-query callback)
-  (let ((url-request-method "POST")
-        (url-request-extra-headers `(("Content-Type" . "application/json")
-                                     ("Authorization" . ,(concat "Bearer " api-key))))
-        (url-request-data
-         (json-encode `(("model" . "gpt-4.0")
-                        ("prompt" . ,(concat "Format your response as a JSON object with two fields. "
-                                             "The first field, 'needs_clarification', should be a boolean indicating whether further clarification or information about the current Emacs state is needed. "
-                                             "The second field, 'response_content', should contain the content of the response, either the Emacs-Lisp code or the clarification question. Here's the query:\n\n"
-                                             "Emacs Environment: " emacs-environment "\n"
-                                             "User Query: " user-query "\n\n"
-                                             "Response:"))
-                        ("temperature" . 0.5)
-                        ("max_tokens" . 250)))))
-    (url-retrieve "https://api.openai.com/v1/completions"
-                  (lambda (_status)
-                    ;; Process the response
-                    (goto-char url-http-end-of-headers)
-                    (let* ((json-object-type 'alist)
-                           (json-array-type 'list)
-                           (json-false nil)
-                           (response (json-read-from-string (buffer-substring-no-properties (point) (point-max)))))
-                      (funcall callback response))
-                    (kill-buffer))
-                  nil t)))
-
-(defun my-callback (response)
-  "Callback function to handle the response."
-  (message "Generated Response (JSON): %s" (json-encode response)))
-
-(defun example-use-of-generate-emacs-lisp-code ()
-  (let ((api-key "your-api-key")  ; Replace with your actual OpenAI API key
-        (emacs-environment "The cursor is currently inside a defun block named 'process-data', which takes two arguments. The buffer contains typical Lisp code structure.")
-        (user-query "Add a third argument named 'verbose' to the 'process-data' function and adjust the function body to print additional information if 'verbose' is true."))
-    (generate-emacs-lisp-code api-key emacs-environment user-query 'my-callback)))
-
 
 
 (provide 'speech-edit)
