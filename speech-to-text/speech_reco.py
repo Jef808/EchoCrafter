d2d73511a9736f1fb70e059f8a481e30e1e598de
Jean-Francois Arbour <jf.arbour@gmail.com>
2023-12-04 08:31:17 -0500

    careful dependency management between ws and pyaudio stream

--- speech-to-text/speech_reco.py
+++ speech-to-text/speech_reco.py
@@ -15,14 +15,6 @@ import time
 import sys
 import signal
 
-# On my system, these are some
-# values of possible values
-# for `input_device_index`
-#
-# index: 13 -----> pipewire
-# index: 14 -----> pulse
-# index: 18 -----> default
-# index: 4  -----> sysdefault
 #
 ##################
 # # Audio config #
@@ -31,12 +23,13 @@ SAMPLE_RATE = 16000
 FRAMES_PER_BUFFER = 3200
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-PIPEWIRE_DEVICE_INDEX = 13
+PIPEWIRE_DEVICE_INDEX = 10
 ##############################
 # # Termination logic config #
 ##############################
 # Global flag indicating when to initiate the termination logic
-_RUNNING = True
+_RUNNING = False
+_SESSION_END_TIME = None
 
 # Timeout in seconds before we fallback to pyaudio.paAbort
 _STREAM_CLEANUP_TIMEOUT = 3
@@ -51,40 +44,13 @@ _WEBSOCKET_IS_ACTIVE = False
 _WEBSOCKET_CLEANUP_TIMEOUT = 3
 
 _LOGGER = None
+ws = None
+
 
 # Set up the signal handler responsible for terminating the program at the end
 def signal_handler(sig, frame):
     global _RUNNING
     _RUNNING = False  # Signal to PyAudio to stop processing
-
-    def timeout_elapsed(start_time, duration):
-        return time.time() - start_time > duration
-
-    # Wait for PyAudio stream to be inactive or timeout
-    time_start = time.time()
-    while stream.is_active() and not timeout_elapsed(time_start, _STREAM_CLEANUP_TIMEOUT):
-        time.sleep(0.1)
-
-    # Attempt to close PyAudio stream
-    # # NOTE: toggling the flag here makes our stream callback
-    # ignore any new data anyway.
-    try:
-        if stream.is_active():
-            _STREAM_CLEANUP_TIMEOUT_PASSED = True
-            stream.stop_stream()
-        stream.close()
-    except Exception as e:
-        print(f"Error on pyaudio stream cleanup: {e}", file=sys.stderr)
-
-    # Signal to AssemblyAI to close the session
-    ws.send(json.dumps({"terminate_session": True}))
-
-    # Wait for WebSocket to close or timeout
-    time_start = time.time()
-    while _WEBSOCKET_IS_ACTIVE and not timeout_elapsed(ws_timeout_start, _WEBSOCKET_CLEANUP_TIMEOUT):
-        time.sleep(0.1)
-
-    p.terminate()  # Terminate PyAudio
 
 
 # Register the signal handler
@@ -92,6 +58,8 @@ signal.signal(signal.SIGINT, signal_handler)
 
 
 # Correctly format and send data to both `ws` and `logger` endpoints
+# NOTE: We will never send data to a websocket before it is ready since
+# the stream is started in the websocket's on_open callback.
 def send_data(ws, in_data, frame_count, time_info, status):
     data = {"audio_data": base64.b64encode(in_data).decode("utf-8")}
     ws.send(json.dumps(data))
@@ -111,33 +79,56 @@ def send_data(ws, in_data, frame_count, time_info, status):
 # This callback is used by pyaudio's stream to handle the collected
 # audio data. It runs in a separate thread.
 def pyaudio_callback(in_data, frame_count, time_info, status):
-    if _RUNNING and _STREAM_CLEANUP_TIMEOUT_PASSED:
-        return (None, pyaudio.paAbort)
+    if not _RUNNING:
+        # At the end of a session, process audio frames until timestamp
+        # is past the session end time, then close websocket
+        global _SESSION_END_TIME
+        if not _SESSION_END_TIME:
+            _SESSION_END_TIME = time_info['current_time']
+        elif time_info['input_buffer_adc_time'] > _SESSION_END_TIME:
+            ws.send(json.dumps({"terminate_session": True}))
+            ws.close()
+            return (None, pyaudio.paComplete)
+    send_data(ws, in_data, frame_count, time_info, status)
+    return (None, pyaudio.paContinue)
 
-    if _WEBSOCKET_IS_ACTIVE:
-        send_data(ws, in_data, frame_count, time_info, status)
 
-    if _RUNNING:
-        return (None, pyaudio.paContinue)
-    elif not _STREAM_CLEANUP_TIMEOUT_PASSED:
-        return (None, pyaudio.paComplete)
-    else:
-        return (None, pyaudio.paAbort)
+#################################
+# Create and start audio stream #
+#################################
 
+p = pyaudio.PyAudio()
+
+def open_stream():
+    return p.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=SAMPLE_RATE,
+        input=True,
+        frames_per_buffer=FRAMES_PER_BUFFER,
+        input_device_index=PIPEWIRE_DEVICE_INDEX,
+        stream_callback=pyaudio_callback)
 
 #################################################################
 # Setup the websocket connecting to the assemblyAI api endpoint #
 #################################################################
 def on_open(ws):
-    global _WEBSOCKET_IS_ACTIVE
-    _WEBSOCKET_IS_ACTIVE = True
+    global stream
+    stream = open_stream()
 
 def on_close(ws, ec, err):
     global _WEBSOCKET_IS_ACTIVE
     _WEBSOCKET_IS_ACTIVE = False
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
 def on_message(ws, msg):
     payload = json.loads(msg)
+    if payload['message_type'] == "SessionBegins":
+        global _WEBSOCKET_IS_ACTIVE
+        _WEBSOCKET_IS_ACTIVE = True
+
     if 'text' in payload and payload['message_type'] == 'FinalTranscript':
         # Print the valuable responses to stdout
         print(payload['text'])
@@ -161,34 +152,12 @@ ws = websocket.WebSocketApp(
         on_close=on_close,
         on_open=on_open)
 
-#################################
-# Create and start audio stream #
-#################################
-
-# Silence a bunch of errors coming from instansiating PyAudio
-_stderr = sys.stderr
-sys.stderr = open(r'/dev/null', 'w')
-
-p = pyaudio.PyAudio()
-
-sys.stderr.close()
-sys.stderr = _stderr
-
-stream = p.open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=SAMPLE_RATE,
-    input=True,
-    frames_per_buffer=FRAMES_PER_BUFFER,
-    input_device_index=PIPEWIRE_DEVICE_INDEX,
-    stream_callback=pyaudio_callback)
-
 #############################################################################
 # Run until the program receives SIGINT, in which case it gracefully exists #
 #############################################################################
 
-with open('logs/stt.log', 'w') as _logger:
-    _LOGGER = _logger
+with open('logs/stt.log', 'w') as _LOGGER:
+    _RUNNING = True
     ec = ws.run_forever()
 
 exit(ec)
