6677831c5ea6127e5c3e753dfacfd67cd61f6fd7
Jean-Francois Arbour <jf.arbour@gmail.com>
2024-04-08 23:44:51 -0400

    Rewrite the docstrings

--- echo_crafter/speech_processor/voice_assistant.py
+++ echo_crafter/speech_processor/voice_assistant.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import math
 import time
 import wave
+import json
 import struct
 from resources import (
     create_recorder,
@@ -20,6 +20,7 @@ from echo_crafter.commander import intent_handler
 from echo_crafter.logger import setup_logger
 from echo_crafter.config import Config
 from echo_crafter.utils import play_sound
+from echo_crafter.speech_processor.utils import utils
 
 logger = setup_logger(__name__)
 
@@ -29,13 +30,13 @@ class VoiceAssistant:
 
     def __init__(self, *,
                  wake_word="Echo-crafter",
-                 wake_word_sensitivity=0.4,
-                 intent_sensitivity=0.6,
+                 wake_word_sensitivity=0.5,
+                 intent_sensitivity=0.5,
                  max_utterance_duration_sec=5.0):
         self.wake_words = [wake_word]
         self.max_utterance_duration_sec = max_utterance_duration_sec
+        self.wake_word_detected_time =  None
         self.audio_buffer = Queue()
-        self.pv = {}
         self.intent_handler = intent_handler.initialize()
         with ExitStack() as stack:
             self.voice_activity_detector = stack.enter_context(create_cobra())
@@ -46,7 +47,7 @@ class VoiceAssistant:
             self.shut_down = stack.pop_all().close
 
     def run(self):
-        """Starts the voice assistant."""
+        """Start the voice assistant in the current thread."""
         try:
             while True:
                 self.reset()
@@ -56,30 +57,28 @@ class VoiceAssistant:
             self.shut_down()
 
     def is_recording(self):
-        """Returns whether the voice assistant is currently recording."""
+        """Check whether the voice assistant is currently recording."""
         return self.recorder.is_recording
 
     def get_frame_length_sec(self):
-        """Returns the frame length in seconds."""
+        """Compute the frame length in seconds in terms of the frame length and the sample rate."""
         return self.recorder.frame_length * 2 / self.recorder.sample_rate
 
-    def wait_for_wake_word(self, num_frames_to_keep: int = 8):
-        """Listens for the wake word and starts processing when detected.
+    def wait_for_wake_word(self, num_frames_to_keep: int = 12):
+        """Listen for the wake word amongst the incoming audio frames.
 
-        Args: num_frames_to_keep: The number of trailing frames to prepend to the next step's buffer
-              in order to not have a gap of "dropped frames" between "wake-word-detected" and beginning of
-              "intent-inference" steps.
+        Args: num_frames_to_keep: The number of trailing frames to add to the buffer once the wake word is detected.
+              This is done in order to not have a gap within the sequence of frames between a "wake-word-detected" event
+              and the beginning of the "intent-inference" step.
 
-        This can be explained as follows: It takes about 10 frames of 1024 bytes for me to say "Echo-crafter" at 16kHz.
-        If say the wake-word detection engine requires 60% of the total audio time of the wake-word to detect it, then
-        detection will actually occur 6 frames after the wake-word is done being spoken. Those frames will be lost unless
-        we save them and prepend them to the next step's buffer.
+        This can be explained as follows: It takes me about 600ms to say "Echo-crafter" (the default wake word). At 1024 bytes per frame and
+        16kHz sample rate, this corresponds to 10 frames. Suppose it takes the wake-word detection engine about 300ms to detect a spoken wake-word. Then
+        we the detection will only happen about 5 frames after the wake-word was spoken. Those frames will be lost, unless we save them and prepend them
+        to the audio buffer for the next processing step.
         """
-        _buffer = deque()
+        _buffer = deque(maxlen=num_frames_to_keep)
+        print("waiting for wake word...")
         while self.is_recording():
-            if len(_buffer) == num_frames_to_keep:
-                _buffer.popleft()
-
             pcm_frame = self.recorder.read()
             _buffer.append(pcm_frame)
 
@@ -88,36 +87,56 @@ class VoiceAssistant:
                 play_sound(Config['WAKE_WORD_DETECTED_WAV'])
                 for frame in _buffer:
                     self.audio_buffer.put_nowait(frame)
+                self.wake_word_detected_time = time.time()
                 break
 
     def wait_for_intent(self, *, save=None):
-        """Infer the intent from the audio frames.
+        """Infer the intent from the incoming audio frames.
 
-        Here we buffer the audio frames in case intent inference
-        fails and we need to transcribe the audio.
+        In a loop, we process the incoming audio frames (from the audio buffer) with our speech-to-intent engine.
+        Once we get an inference, we check if it is understood. If it is, we handle the intent and slots.
+        If not, we transcribe the audio until the end of the user's utterance and handle the transcription
+        which is sent to the transcription handler.
         """
-        while self.is_recording():
-            pcm_frame = self.recorder.read()
-            self.audio_buffer.put_nowait(pcm_frame)
+        print("waiting for intent...")
+        with self.audio_buffering():
+            while self.is_recording():
+                pcm_frame = self.audio_buffer.get()
 
-            if self.speech_to_intent.process(pcm_frame):
-                inference = self.speech_to_intent.get_inference()
-                if inference.is_understood:
-                    play_sound(Config['INTENT_SUCCESS_WAV'])
-                    self.handle_intent(inference)
-                else:
-                    with self.audio_buffering():
-                        utterance = self.get_utterance()
+                if self.speech_to_intent.process(pcm_frame):
+                    inference = self.speech_to_intent.get_inference()
+                    if inference.is_understood:
+                        print(json.dumps(inference, indent=2))
+                        play_sound(Config['INTENT_SUCCESS_WAV'])
+                        self.handle_intent(inference)
+                    else:
+                        print("Intent not understood, transcribing...")
+                        stop_event = lambda: (
+                            (self.wake_word_detected_time and
+                             time.time() - self.wake_word_detected_time > self.max_utterance_duration_sec) or
+                            self.audio_buffer.empty() and not self.is_recording()
+                        )
+                        utterance = utils.get_utterance(
+                            audio_buffer=self.audio_buffer,
+                            vad=self.voice_activity_detector,
+                            frame_length=self.recorder.frame_length,
+                            sample_rate=self.recorder.sample_rate,
+                            stop_event=stop_event
+                        )
                         transcript, words = self.speech_to_text.process(utterance)
                         print("Got transcription...")
                         if save:
                             self.save_utterance(utterance, save)
                         self.handle_transcription(transcript, words)
-                break
+                    break
 
     @contextmanager
     def audio_buffering(self):
-        """Buffers audio frames for processing in a separate thread."""
+        """Buffer incoming audio frames in a separate thread.
+
+        This is useful for processing audio frames from a buffer and backtracking to previous
+        frames in case we want to (e.g. in case of a failed intent inference).
+        """
         stop_event = Event()
 
         def _do_buffer_audio():
@@ -135,80 +154,58 @@ class VoiceAssistant:
             if t.is_alive():
                 raise RuntimeError("Failed to stop buffering thread")
 
-    def get_utterance(self) -> list[int]:
-        """Detect the end of speech.
-
-        This function uses VAD to detect the end of speech.
-        Returns the number of frames during which speech was detected.
-        """
-        utterance = list()
-
-        # Compute the number of silent frames to wait before ending the utterance
-        frame_length_sec = self.get_frame_length_sec()
-        num_frames = math.ceil(Config['ENDPOINT_DURATION_SEC'] / frame_length_sec)
-
-        n_silent_frames = -1
-        time_start = time.time()
-
-        while self.is_recording():
-            utterance.extend(self.audio_buffer.get())
-            voice_probability = self.voice_activity_detector.process(utterance[-self.recorder.frame_length:])
-
-            if n_silent_frames >= 0 and voice_probability < 0.1:
-                n_silent_frames += 1
-            elif voice_probability > 0.12:
-                n_silent_frames = 0
-
-            if (n_silent_frames == num_frames
-                or (n_silent_frames < 0 and time.time() - time_start > self.max_utterance_duration_sec)
-            ):
-                break
-        return utterance
-
-
     def reset(self):
-        """Resets the state of the voice assistant.
+        """Reset the state of the voice assistant.
 
-        Note: it is necessary to stop and start the recorder instance in order to
-        flush its inner circular buffer.
-        """
+        This includes the speech-to-intent engine's internal state, the audio buffer,
+        the recorder's internal buffer, along with any variables used for tracking
+        the speech processor's current state."""
         self.speech_to_intent.reset()
         self._pause_recorder()
         self._flush_audio_buffer()
         self._resume_recorder()
+        self.wake_word_detected_time = None
 
     def _pause_recorder(self):
-        """Pauses the recorder."""
+        """Pause the recorder.
+
+        Note: When pausing and resuming the recorder, its internal buffer gets flushed."""
         if self.is_recording():
             self.recorder.stop()
         while self.is_recording():
             pass
 
     def _resume_recorder(self):
-        """Resumes the recorder."""
+        """Resume the recorder.
+
+        Note: When pausing and resuming the recorder, its internal buffer gets flushed."""
         if not self.is_recording():
             self.recorder.start()
 
     def _flush_audio_buffer(self):
-        """Flushes the audio buffer."""
-        while True:
-            try:
-                self.audio_buffer.get_nowait()
-            except Empty:
-                break
+        """Flush the audio buffer."""
+        self.audio_buffer.queue.clear()
 
     def save_utterance(self, utterance: list[int], file_path: str) -> None:
-        """Saves the utterance to a WAV file."""
+        """Save the utterance to a WAV file.
+
+        Note: The utterance is a list of regular integers, but the format at which we sample
+        audio and save it to the WAV file is as a sequence of signed 16-bit integers.
+        Therefore, we need to pack them two at a time before writing them to the WAV file."""
         with wave.open(file_path, 'wb') as wf:
             wf.setparams((1, 2, 16000, 512, "NONE", "NONE"))
             wf.writeframes(struct.pack("h" * len(utterance), *utterance))
 
     def handle_intent(self, inference):
-        """Handles the processing of recognized intents."""
+        """Handle a recognized speech-to-intent inference.
+
+        We pass the intent and slots as they are generated by the speech-to-intent engine.
+        Further processing is done within the intent handler.
+        """
         self.intent_handler(intent=inference.intent, slots=inference.slots)
 
     def handle_transcription(self, transcript, words):
-        """Handles the processing of transcriptions."""
+        """Handle a transcription for the speech that was not recognized by the speech-to-intent engine."""
         print(transcript)
         print(tabulate(
             words,
